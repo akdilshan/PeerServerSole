462ab10e37467a82211690872ea7815b0ce14b30
akdilshan <dilshanamarasinghe1@gmail.com>
2019-01-21 10:34:03 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+var PeerServer = require('peer').PeerServer;
+var server = PeerServer({port: 9000, path: '/myapp'});
+console.log("Server Started");
+
+// var fs = require('fs');
+// var PeerServer= require('peer').PeerServer;
+
+// var server=PeerServer({
+//     port:9000,
+//     ssl:{
+//         key: fs.readFileSync('./key.pem'),
+//         cert:fs.readFileSync('./cert.pem')
+//     },
+//     path: '/myapp'
+// });
+// console.log("Server Started");
+
+server.on('connection', function(id) { 
+    console.log(id);
+ });
+
+ server.on('disconnect', function(id) { 
+    console.log(id);
+  });
